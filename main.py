10a7667ef252a020a220c09cdca26afd41cd4e79
mrstarbutterfly <nastya.silch2003@gmail.com>
2022-12-15 22:47:21 +0300

    update

--- main.py
+++ main.py
@@ -21,20 +21,29 @@ def create_graph(df):
         plt.savefig(f'images/male_{col}.jpg')
         plt.clf()
 
+    plt.hist(df['Genre'])
+    plt.xlabel('Genre')
+    plt.ylabel('Num of men and women')
+    plt.savefig(f'images/male_genre.jpg')
+
+
 
 
 def check(df):
     count_of_female = len(df[(df['Genre'] == 'Female') & (df['Annual Income (k$)'] > 61.5)])
     count_of_male = len(df[(df['Genre'] == 'Male') & (df['Annual Income (k$)'] > 61.5)])
+
     if count_of_female>count_of_male:
         return True
     else:
         return False
 
+
 def main():
     df = create_df()
+    print(df.head())
     create_graph(df)
-    print("My hypothesis is women spends more money than men yearly. "
+    print("\nMy hypothesis is women spends more money than men yearly. "
           "We check it on the dataset about mall customers \n")
     print(f'num of man: {len(df[df["Genre"] == "Male"])} and num of woman: {len(df[df["Genre"] == "Female"])}')
     print("Number of men and women almost equal\n")
